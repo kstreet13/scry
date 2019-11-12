8ab02682c50dd2723548716bf425dad2d552f17a
Kelly Street <kstreet@root-pcs-air.dfci.harvard.edu>
2019-11-12 11:41:49 -0500

    setup GLMPCA function (renamed from runGLMPCA) with three methods (SCE, SE, matrix), added generic and documentation. Ran devtools::document(), which added NAMESPACE. Renamed package untitledsinglecell (no underscore).

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,3 +1,12 @@
 
-# runGLMPCA
+#' @title GLM-PCA
+#' @name GLMPCA
+#' @export
+setGeneric(
+	name = "GLMPCA",
+	signature = 'object',
+	def = function(object, ...) {
+		standardGeneric("GLMPCA")
+	}
+)
 
--- R/glmpca.R
+++ R/glmpca.R
@@ -1,10 +1,15 @@
 
-#' @title GLM-PCA
+#' @rdname GLMPCA
 #' @description This function implements the GLM-PCA dimensionality reduction
-#'   method for high-dimensional count data stored in a SingleCellExperiment.
-#' @name runGLMPCA
+#'   method for high-dimensional count data. This is a wrapper for
+#'   \code{\link[glmpca]{glmpca}}.
 #' 
-#' @param object A \link{SingleCellExperiment} object.
+#' @param object A \code{\link{SingleCellExperiment}} or
+#'   \link{SummarizedExperiment} object. Alternatively, a matrix of integer
+#'   counts.
+#' @param assay a character or integer specifying which assay to use for GLM-PCA
+#'   (default = 1). Ignored if \code{object} is a matrix.
+#'
 #' @param L the desired number of latent dimensions (integer).
 #' @param fam character describing the likelihood to use for the data (poisson,
 #'   negative binomial, binomial approximation to multinomial, bernoulli).
@@ -13,8 +18,8 @@
 #'   Regression coefficients are not penalized.
 #' @param verbose logical value indicating whether the current deviance should
 #'   be printed after each iteration (default = FALSE).
-#' @param init a list containing initial estimates for the factors (\code{U}) and
-#'   loadings (\code{V}) matrices.
+#' @param init a list containing initial estimates for the factors (\code{U})
+#'   and loadings (\code{V}) matrices.
 #' @param nb_theta see \code{\link[MASS]{negative.binomial}} (nb_theta -> infty
 #'   = Poisson).
 #' @param X a matrix of column (observations) covariates. Any column with all
@@ -26,117 +31,76 @@
 #' @details The basic model is \code{R = AX\'+ZG\'+VU\'}, where \code{E\[Y\] = M
 #'   = linkinv(R)}. Regression coefficients are \code{A} and \code{G}, latent
 #'   factors are \code{U} and loadings are \code{V}.
-#' 
-#' @return A list containing:
+#'
+#' @return The original \code{SingleCellExperiment} or
+#'   \code{SummarizedExperiment} object, with the GLM-PCA results added to the
+#'   \code{metadata} slot. If the original input was a
+#'   \code{SingleCellExperiment}, then a new \code{reducedDim} element called
+#'   \code{"GLMPCA"} will be added, representing the GLM-PCA \code{factors}. If
+#'   the input was a matrix, output matches that of
+#'   \code{\link[glmpca]{glmpca}}.
+#'   
 #' 
 #' @importFrom glmpca glmpca
+#' @importFrom SingleCellExperiment reducedDim<-
 #' @export
-glmpca <- function(Y, L, fam=c("poi","nb","mult","bern"),
-				 ctl = list(maxIter=1000, eps=1e-4),
-				 penalty = 1, verbose = FALSE,
-				 init = list(factors=NULL, loadings=NULL),
-				 nb_theta = 1, X = NULL, Z = NULL, sz = NULL){
-  #Y is data with features=rows, observations=cols
-  #L is number of desired latent dimensions
-  #fam the likelihood for the data 
-  #(poisson, negative binomial, binomial approximation to multinomial, bernoulli)
-  #ctl a list of control parameters for optimization
-  #penalty the L2 penalty for the latent factors
-  #regression coefficients are not penalized
-  #nb_theta see MASS::negative.binomial (nb_theta->infty = Poisson)
-  #X a matrix of column (observations) covariates
-  #any column with all same values (eg 1 for intercept) will be removed
-  #this is because we force the intercept, so want to avoid collinearity
-  #Z a matrix of row (feature) covariates, usually not needed
-  #the basic model is R=AX'+ZG'+VU', where E[Y]=M=linkinv(R)
-  #regression coefficients are A,G, latent factors are U and loadings V.
-  
-  fam<-match.arg(fam)
-  N<-ncol(Y); J<-nrow(Y)
-  #sanity check inputs
-  if(fam %in% c("poi","nb","mult","bern")){ stopifnot(min(Y) >= 0) }
-  if(fam=="bern"){ stopifnot(max(Y) <= 1) }
-  
-  #preprocess covariates and set updateable indices
-  if(!is.null(X)){ 
-    stopifnot(nrow(X)==ncol(Y))
-    #we force an intercept, so remove it from X to prevent collinearity
-    X<-remove_intercept(X)
-    Ko<-ncol(X)+1
-  } else {
-    Ko<-1
-  }
-  if(!is.null(Z)){ 
-    stopifnot(nrow(Z)==nrow(Y)) 
-    Kf<-ncol(Z)
-  } else {
-    Kf<-0
-  }
-  lid<-(Ko+Kf)+(1:L)
-  uid<-Ko + 1:(Kf+L)
-  vid<-c(1:Ko, lid)
-  Ku<-length(uid); Kv<-length(vid)
-  
-  #create glmpca_family object
-  gnt<-glmpca_init(Y,fam,sz,nb_theta)
-  gf<-gnt$gf; rfunc<-gnt$rfunc; a1<-gnt$intercepts
-  
-  #initialize U,V, with row-specific intercept terms
-  U<-cbind(1, X, matrix(rnorm(N*Ku)*1e-5/Ku,nrow=N))
-  if(!is.null(init$factors)){
-    #print("initialize factors")
-    L0<-min(L,ncol(init$factors))
-    U[,(Ko+Kf)+(1:L0)]<-init$factors[,1:L0,drop=FALSE]
-  }
-  #a1 = naive MLE for gene intercept only
-  V<-cbind(a1, matrix(rnorm(J*(Ko-1))*1e-5/Kv,nrow=J))
-  V<-cbind(V, Z, matrix(rnorm(J*L)*1e-5/Kv,nrow=J))
-  if(!is.null(init$loadings)){
-    #print("initialize loadings")
-    L0<-min(L,ncol(init$loadings))
-    V[,(Ko+Kf)+(1:L0)]<-init$loadings[,1:L0,drop=FALSE]
-  }
-  
-  #run optimization
-  dev<-rep(NA,ctl$maxIter)
-  for(t in 1:ctl$maxIter){
-    #rmse[t]<-sd(Y-ilfunc(rfunc(U,V)))
-    dev[t]<-gf$dev_func(Y,rfunc(U,V))
-    if(t>5 && abs(dev[t]-dev[t-1])/(0.1+abs(dev[t-1]))<ctl$eps){
-      break
-    }
-    if(verbose){ 
-      dev_format<-format(dev[t],scientific=TRUE,digits=4)
-      print(paste0("Iteration: ",t," | deviance=",dev_format)) 
-    }
-    #(k %in% lid) ensures no penalty on regression coefficients: 
-    for(k in vid){
-      ig<- gf$infograd(Y,rfunc(U,V))
-      grads<- (ig$grad)%*%U[,k] - penalty*V[,k]*(k %in% lid) 
-      infos<- (ig$info) %*% U[,k]^2 + penalty*(k %in% lid)
-      V[,k]<-V[,k]+grads/infos
-    }
-    for(k in uid){
-      ig<- gf$infograd(Y,rfunc(U,V))
-      grads<- crossprod(ig$grad, V[,k]) - penalty*U[,k]*(k %in% lid) 
-      infos<- crossprod(ig$info, V[,k]^2) + penalty*(k %in% lid) 
-      U[,k]<-U[,k]+grads/infos
-    }
-  }
-  #postprocessing: include row and columnn labels for regression coefficients
-  if(is.null(Z)){
-    G<-NULL
-  } else {
-    G<-U[,Ko+(1:Kf),drop=FALSE]
-    rownames(G)<-colnames(Y); colnames(G)<-colnames(Z)
-  }
-  X<-if(is.null(X)){ matrix(1,nrow=N) } else { cbind(1,X) }
-  if(!is.null(colnames(X))){ colnames(X)[1]<-"(Intercept)" }
-  A<-V[,1:Ko,drop=FALSE]
-  rownames(A)<-rownames(Y); colnames(A)<-colnames(X)
-  res<-ortho(U[,lid],V[,lid],A,X=X,G=G,Z=Z,ret="df")
-  rownames(res$factors)<-colnames(Y)
-  rownames(res$loadings)<-rownames(Y)
-  res$dev=dev[1:t]; res$family<-gf
-  res
-}
+setMethod(f = "GLMPCA",
+		  signature = signature(object = "SingleCellExperiment"),
+		  definition = function(object, assay = 1,
+		  					  L, fam=c("poi","nb","mult","bern"),
+		  					  ctl = list(maxIter=1000, eps=1e-4),
+		  					  penalty = 1, verbose = FALSE,
+		  					  init = list(factors=NULL, loadings=NULL),
+		  					  nb_theta = 1, X = NULL, Z = NULL, sz = NULL){
+		  	
+		  	res <- glmpca(Y = assay(object, assay),
+		  				  L = L, fam = fam, ctl = ctl, penalty = penalty,
+		  				  verbose = verbose, init = init, nb_theta = nb_theta,
+		  				  X = X, Z = Z, sz = sz)
+		  	
+		  	reducedDim(object, "GLMPCA") <- res$factors
+		  	res$factors <- NULL
+		  	object@metadata$glmpca <- res
+		  	return(object)
+		  })
+
+#' @rdname GLMPCA
+#' @importFrom glmpca glmpca
+#' @export
+setMethod(f = "GLMPCA",
+		  signature = signature(object = "SummarizedExperiment"),
+		  definition = function(object, assay = 1,
+		  					  L, fam=c("poi","nb","mult","bern"),
+		  					  ctl = list(maxIter=1000, eps=1e-4),
+		  					  penalty = 1, verbose = FALSE,
+		  					  init = list(factors=NULL, loadings=NULL),
+		  					  nb_theta = 1, X = NULL, Z = NULL, sz = NULL){
+		  	
+		  	res <- glmpca(Y = assay(object, assay),
+		  				  L = L, fam = fam, ctl = ctl, penalty = penalty,
+		  				  verbose = verbose, init = init, nb_theta = nb_theta,
+		  				  X = X, Z = Z, sz = sz)
+		  	
+		  	object@metadata$glmpca <- res
+		  	return(object)
+		  })
+
+#' @rdname GLMPCA
+#' @importFrom glmpca glmpca
+#' @export
+setMethod(f = "GLMPCA",
+		  signature = signature(object = "matrix"),
+		  definition = function(object, assay = 1,
+		  					  L, fam=c("poi","nb","mult","bern"),
+		  					  ctl = list(maxIter=1000, eps=1e-4),
+		  					  penalty = 1, verbose = FALSE,
+		  					  init = list(factors=NULL, loadings=NULL),
+		  					  nb_theta = 1, X = NULL, Z = NULL, sz = NULL){
+		  	
+		  	res <- glmpca(Y = assay(object, assay),
+		  				  L = L, fam = fam, ctl = ctl, penalty = penalty,
+		  				  verbose = verbose, init = init, nb_theta = nb_theta,
+		  				  X = X, Z = Z, sz = sz)
+		  	
+		  	return(res)
+		  })
