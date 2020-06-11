d68f85818e2266eb613e4bc87971b8a4b3be0359
Davide Risso <risso.davide@gmail.com>
2020-06-11 18:43:10 +0200

    Fixed small bug

--- R/nullResiduals.R
+++ R/nullResiduals.R
@@ -3,12 +3,12 @@
     # adapted from compute_size_factors
     lsz <- log(DelayedArray::colSums(m))
     sz <- exp(lsz-mean(lsz))
-    
+
     # adapted from .null_residuals
     lambdahat <- DelayedArray::rowSums(m) / sum(sz)
     mhat <- DelayedArray(matrix(lambdahat)) %*%
         DelayedArray(matrix(sz, nrow = 1))
-    
+
     # adapted from .poisson_deviance_residuals
     term1 <- m * log(m / mhat)
     term1[is.na(term1)] <- 0 #0*log(0)=0
@@ -20,14 +20,14 @@
     #X a matrix, n is vector of length ncol(X)
     stopifnot(length(n) == ncol(X))
     if(is.matrix(X)){ #X is a dense matrix
-        if(length(p) == nrow(X)){ 
+        if(length(p) == nrow(X)){
             #p is a vector, length must match nrow(X)
             mu <- outer(p, n)
         } else if(!is.null(dim(p)) && dim(p) == dim(X)){
             # p is matrix, must have same dims as X
             mu <- t(t(p)*n)
-        } else { 
-            stop("dimensions of p and X must match!") 
+        } else {
+            stop("dimensions of p and X must match!")
         }
         term1 <- X*log(X/mu)
         term1[is.na(term1)] <- 0 #0*log(0)=0
@@ -35,7 +35,7 @@
         term2 <- nx*log(nx/outer(1-p, n))
         #this next line would only matter if all counts
         #were from a single gene, so not checking saves time.
-        # term2[is.na(term2)] <- 0 
+        # term2[is.na(term2)] <- 0
         return(sign(X-mu)*sqrt(2*(term1+term2)))
     } else { #X is a sparse Matrix or delayed Array
         stopifnot(length(p) == nrow(X))
@@ -49,7 +49,7 @@
             term2 <- nx*log(nx/(n*(1-p[j])))
             #this next line would only matter if all counts
             #were from a single gene, so not checking saves time.
-            # term2[is.na(term2)] <- 0 
+            # term2[is.na(term2)] <- 0
             sign(x-mu)*sqrt(2*(term1+term2))
         }
         #up to this point no large dense objects have been created
@@ -70,7 +70,7 @@
     sign(x-xhat)*sqrt(abs(s2))
 }
 
-.null_residuals <- function(m, fam = c("binomial", "poisson"), 
+.null_residuals <- function(m, fam = c("binomial", "poisson"),
                            type = c("deviance", "pearson")){
     #m is a matrix, sparse Matrix, or delayed Array
     fam <- match.arg(fam); type <- match.arg(type)
@@ -92,7 +92,7 @@
                     res[is.na(res)] <- 0 #case of 0/0
                 }
                 #this last line is the only part where the full dense object
-                #is instantiated in memory, replace with row-by-row write to 
+                #is instantiated in memory, replace with row-by-row write to
                 #delayedArray
                 return(t(vapply(seq_len(nrow(m),pr_func,FUN.VALUE=0.0*sz))))
             } #end sparse binomial Pearson block
@@ -100,8 +100,8 @@
     } else { #fam == "poisson"
         lambda<-rowSums(m)/sum(sz)
         if(is.matrix(m)){ #dense data matrix
-            mhat <- outer(lambda, sz) 
-            if(type == "deviance"){ 
+            mhat <- outer(lambda, sz)
+            if(type == "deviance"){
                 return(.poisson_deviance_residuals(m, mhat))
             } else { #pearson residuals
                 res <- (m-mhat)/sqrt(mhat)
@@ -122,7 +122,7 @@
                     res
                 }
             }
-            #up to this point no dense objects created in memory, 
+            #up to this point no dense objects created in memory,
             #modify below line
             #to write each row to a disk based delayedArray
             return(t(vapply(seq_len(nrow(m)),rfunc,FUN.VALUE=0.0*sz)))
@@ -154,9 +154,9 @@
 #'   residuals matrix can be analyzed with standard PCA as a fast approximation
 #'   to GLM-PCA.
 #'
-#' @param object The object on which to compute residuals. It can be a 
-#'   matrix-like object (e.g. matrix, Matrix, DelayedMatrix, HDF5Matrix) with 
-#'   genes int he rows and samples in the columns. Specialized methods are 
+#' @param object The object on which to compute residuals. It can be a
+#'   matrix-like object (e.g. matrix, Matrix, DelayedMatrix, HDF5Matrix) with
+#'   genes int he rows and samples in the columns. Specialized methods are
 #'   defined for objects inheriting from \link{SummarizedExperiment} (such as
 #'   \code{\link{SingleCellExperiment}}).
 #' @param assay a string or integer specifying which assay contains the count
@@ -169,60 +169,60 @@
 #' @param batch an optional factor indicating batch membership of observations.
 #'   If provided, the null model is computed within each batch separately to
 #'   regress out the batch effect from the resulting residuals.
-#'  
+#'
 #' @return The original \code{SingleCellExperiment} or
 #'   \code{SummarizedExperiment} object with the residuals appended as a new
 #'   assay. The assay name will be fam_type_residuals (eg,
 #'   binomial_deviance_residuals). If the input was a matrix, output is a dense
 #'   matrix containing the residuals.
-#'  
+#'
 #' @details This function should be used only on the un-normalized counts.
-#'  It was originally designed for single-cell RNA-seq counts 
-#'  obtained by the use of unique molecular identifiers (UMIs) and has not been 
+#'  It was originally designed for single-cell RNA-seq counts
+#'  obtained by the use of unique molecular identifiers (UMIs) and has not been
 #'  tested on read count data without UMIs or other data types.
-#'  
-#'  Note that even though sparse Matrix objects are accepted as input, 
-#'  they are internally coerced to dense matrix before processing, 
+#'
+#'  Note that even though sparse Matrix objects are accepted as input,
+#'  they are internally coerced to dense matrix before processing,
 #'  because the output
-#'  is always a dense matrix since the residuals transformation 
+#'  is always a dense matrix since the residuals transformation
 #'  is not sparsity preserving.
 #'  To avoid memory issues, it is recommended to perform feature selection first
-#'  and subset the number of features to a smaller size prior to computing the 
+#'  and subset the number of features to a smaller size prior to computing the
 #'  residuals.
-#'  
+#'
 #' @references Townes FW, Hicks SC, Aryee MJ, and Irizarry RA (2019). Feature
 #' Selection and Dimension Reduction for Single Cell RNA-Seq based on a
 #' Multinomial Model. \emph{Genome Biology}
 #' \url{https://doi.org/10.1186/s13059-019-1861-6}
-#' 
+#'
 #' @examples
 #' ncells <- 100
 #' u <- matrix(rpois(20000, 5), ncol=ncells)
 #' sce <- SingleCellExperiment::SingleCellExperiment(assays=list(counts=u))
 #' nullResiduals(sce)
-#' 
+#'
 #' @importFrom SummarizedExperiment assay
-#' @importFrom SummarizedExperiment assay<- 
+#' @importFrom SummarizedExperiment assay<-
 #' @export
-setMethod(f = "nullResiduals", 
-          signature = signature(object = "SummarizedExperiment"), 
+setMethod(f = "nullResiduals",
+          signature = signature(object = "SummarizedExperiment"),
           definition = function(object, assay = 1,
-                                fam = c("binomial", "poisson"), 
-                                type = c("deviance", "pearson"), 
+                                fam = c("binomial", "poisson"),
+                                type = c("deviance", "pearson"),
                                 batch = NULL){
               fam <- match.arg(fam); type <- match.arg(type)
               # m <- as.matrix(assay(object, assay))
               name <- paste(fam, type, "residuals", sep="_")
-              assay(object, name) <- .null_residuals_batch(m, fam, type, batch)
+              assay(object, name) <- .null_residuals_batch(assay(object, assay), fam, type, batch)
               object
           })
 
 #' @rdname nullResiduals
 #' @export
-setMethod(f = "nullResiduals", 
-          signature = signature(object = "matrix"), 
-          definition = function(object, fam = c("binomial", "poisson"), 
-                                type = c("deviance", "pearson"), 
+setMethod(f = "nullResiduals",
+          signature = signature(object = "matrix"),
+          definition = function(object, fam = c("binomial", "poisson"),
+                                type = c("deviance", "pearson"),
                                 batch = NULL){
               fam <- match.arg(fam); type <- match.arg(type)
               .null_residuals_batch(object, fam, type, batch)
@@ -230,10 +230,10 @@ setMethod(f = "nullResiduals",
 
 #' @rdname nullResiduals
 #' @export
-setMethod(f = "nullResiduals", 
-          signature = signature(object = "Matrix"), 
-          definition = function(object, fam = c("binomial", "poisson"), 
-                                type = c("deviance", "pearson"), 
+setMethod(f = "nullResiduals",
+          signature = signature(object = "Matrix"),
+          definition = function(object, fam = c("binomial", "poisson"),
+                                type = c("deviance", "pearson"),
                                 batch = NULL){
               fam <- match.arg(fam); type <- match.arg(type)
               #.null_residuals_batch(as.matrix(object), fam, type, batch)
@@ -243,10 +243,10 @@ setMethod(f = "nullResiduals",
 #' @rdname nullResiduals
 #' @export
 #' @importClassesFrom DelayedArray DelayedMatrix
-setMethod(f = "nullResiduals", 
-          signature = signature(object = "ANY"), 
-          definition = function(object, fam = c("binomial", "poisson"), 
-                                type = c("deviance", "pearson"), 
+setMethod(f = "nullResiduals",
+          signature = signature(object = "ANY"),
+          definition = function(object, fam = c("binomial", "poisson"),
+                                type = c("deviance", "pearson"),
                                 batch = NULL){
               if(!is(x, "HDF5Matrix") && !is(x, "DelayedMatrix")) {
                   stop("x is of type ", class(x), ", currently not supported")
